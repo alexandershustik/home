c7ead699fea5bca57a0aa4a501195f0e99994a5a
alexandershustik <alexandershustik@gmail.com>
2017-02-28 08:37:32 +0200

    верстка слайдера

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,7 @@
 var slideDomCount = $('.slides img').length;
 activeSlide = 0;
 
-//interval =  setInterval( changeSlide, 4000);
+interval =  setInterval( changeSlide, 4000);
 
 function changeSlide(){
 	slideDomEl = '.slides img[data-slide=' + activeSlide + ']';
@@ -12,30 +12,29 @@ function changeSlide(){
 	$('.slides img').removeClass('active');
 	$('.slides img').removeClass('left');
 	$('.lead-text').removeClass('active');
-	$('.lead-text').removeClass('left');
 
 	$(slideDomEl).addClass('active');
 	$(slideDomEl2).addClass('active');
 
-	//(activeSlide <= 0) ? $('.arrows-wrap span.left').attr('data-slide', slideDomCount - 1) : $('.arrows-wrap span.left').attr('data-slide', activeSlide - 1);
-	//(activeSlide >= slideDomCount - 1) ? $('.arrows-wrap span.right').attr('data-slide', 0) : $('.arrows-wrap span.right').attr('data-slide', parseInt(activeSlide) + 1);
+	(activeSlide <= 0) ? $('.slider-arr-wrap.left').attr('data-slide', slideDomCount - 1) : $('.slider-arr-wrap.left').attr('data-slide', activeSlide - 1);
+	(activeSlide >= slideDomCount - 1) ? $('.slider-arr-wrap.right').attr('data-slide', 0) : $('.slider-arr-wrap.right').attr('data-slide', parseInt(activeSlide) + 1);
 
 	(activeSlide <= 0) ? $('.slides img:last-child').addClass('left') : $(slideDomEl).prev().addClass('left');
-	(activeSlide <= 0) ? $('.lead-text:last-child').addClass('left') : $(slideDomEl2).prev().addClass('left');
+	//(activeSlide <= 0) ? $('.lead-text:last-child').addClass('left') : $(slideDomEl2).prev().addClass('left');
 
 	(activeSlide < slideDomCount - 1) ? activeSlide++ : activeSlide = 0;
 
 }
 changeSlide();
 
-$('.arrows-wrap span.left').click(function(){
-	activeSlide = $('.arrows-wrap span.left').attr('data-slide');
+$('.slider-arr-wrap.left').click(function(){
+	activeSlide = $('.slider-arr-wrap.left').attr('data-slide');
 	interval = clearInterval(interval);
 	changeSlide();
 	interval =  setInterval( changeSlide, 4000);
 });
-$('.arrows-wrap span.right').click(function(){
-	activeSlide = $('.arrows-wrap span.right').attr('data-slide');
+$('.slider-arr-wrap.right').click(function(){
+	activeSlide = $('.slider-arr-wrap.right').attr('data-slide');
 	interval = clearInterval(interval);
 	changeSlide();
 	interval =  setInterval( changeSlide, 4000);
